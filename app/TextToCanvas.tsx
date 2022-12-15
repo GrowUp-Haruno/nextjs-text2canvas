2b98929c61af50f0b10b4a92d41a238b128f3af4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-15 20:47:11 +0900

    変換コンポーネントを追加

---
+++ app/TextToCanvas.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { Canvas } from './Canvas';
+
+export function TextToCanvas() {
+  const maxNameLength = 20;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [inputText, setInputText] = useState<string>('');
+  const [textPath, setTextPath] = useState<TPath | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setTextPath(await getPath(inputText));
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const changeInput: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
+    const regex = /[&'`"<>]/g;
+    if (regex.test(e.target.value)) return;
+    if (!(e.target.value.length <= maxNameLength)) return;
+
+    setInputText(() => e.target.value);
+  };
+
+  const changeText2Path: React.MouseEventHandler<
+    HTMLButtonElement
+  > = async () => {
+    setIsLoading(true);
+    setTextPath(await getPath(inputText));
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={changeInput}
+        value={inputText}
+        disabled={isLoading}
+      />
+      <button onClick={changeText2Path} disabled={isLoading}>
+        変換
+      </button>
+      <br />
+      <Canvas textPath={textPath} />
+    </div>
+  );
+}
+
+const getPath = async (text?: string) => {
+  const res = await fetch('http://localhost:3000/api/text2path', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text }),
+  });
+
+  if (!res.ok) {
+    throw new Error('フェッチに失敗しました');
+  }
+
+  const { path } = (await res.json()) as {
+    path: TPath;
+  };
+
+  return path;
+};
+
+export type TPath = {
+  commands: opentype.PathCommand[];
+  fill: string | null;
+  stroke: string | null;
+  strokeWidth: number;
+};
