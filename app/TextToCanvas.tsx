62cf169d0ca91e6620acf563c50b27e1e2f312cf
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-15 22:05:07 +0900

    ロジック分離

--- app/TextToCanvas.tsx
+++ app/TextToCanvas.tsx
@@ -1,36 +1,12 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { memo } from 'react';
 import { Canvas } from './Canvas';
+import { useTextToCanvas } from './hook';
 import { TextInput } from './TextInput';
 
-export function TextToCanvas() {
-  const maxNameLength = 20;
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [inputText, setInputText] = useState<string>('');
-  const [textPath, setTextPath] = useState<TPath | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      setTextPath(await getPath(inputText));
-      setIsLoading(false);
-    })();
-  }, []);
-
-  const changeInput: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
-    const regex = /[&'`"<>]/g;
-    if (regex.test(e.target.value)) return;
-    if (!(e.target.value.length <= maxNameLength)) return;
-
-    setInputText(() => e.target.value);
-  };
-
-  const changeText2Path: React.MouseEventHandler<
-    HTMLButtonElement
-  > = async () => {
-    setIsLoading(true);
-    setTextPath(await getPath(inputText));
-    setIsLoading(false);
-  };
+export const TextToCanvas = memo(() => {
+  const { inputText, isLoading, textPath, changeInput, changeText2Path } =
+    useTextToCanvas();
 
   return (
     <div>
@@ -43,27 +19,7 @@ export function TextToCanvas() {
       <Canvas textPath={textPath} isLoading={isLoading} />
     </div>
   );
-}
-
-const getPath = async (text?: string) => {
-  const res = await fetch('http://localhost:3000/api/text2path', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ text }),
-  });
-
-  if (!res.ok) {
-    throw new Error('フェッチに失敗しました');
-  }
-
-  const { path } = (await res.json()) as {
-    path: TPath;
-  };
-
-  return path;
-};
+});
 
 export type TPath = {
   commands: opentype.PathCommand[];
---
+++ app/hook.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { TPath } from './TextToCanvas';
+
+export const useTextToCanvas = () => {
+  const maxNameLength = 20;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [inputText, setInputText] = useState<string>('');
+  const [textPath, setTextPath] = useState<TPath | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setTextPath(await getPath(inputText));
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const changeInput: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
+    const regex = /[&'`"<>]/g;
+    if (regex.test(e.target.value)) return;
+    if (!(e.target.value.length <= maxNameLength)) return;
+
+    setInputText(() => e.target.value);
+  };
+
+  const changeText2Path: React.MouseEventHandler<
+    HTMLButtonElement
+  > = async () => {
+    setIsLoading(true);
+    setTextPath(await getPath(inputText));
+    setIsLoading(false);
+  };
+
+  return { inputText, isLoading, textPath, changeInput, changeText2Path };
+};
+
+const getPath = async (text?: string) => {
+  const res = await fetch('http://localhost:3000/api/text2path', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text }),
+  });
+
+  if (!res.ok) {
+    throw new Error('フェッチに失敗しました');
+  }
+
+  const { path } = (await res.json()) as {
+    path: TPath;
+  };
+
+  return path;
+};
