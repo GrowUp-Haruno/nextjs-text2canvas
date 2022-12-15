3d1842cf207924cca4a0b7279a6167b739778842
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-15 21:50:56 +0900

    パスの描画設定を修正

--- pages/api/text2path.ts
+++ pages/api/text2path.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import opentype from 'opentype.js';
+
 import { TPath } from '../../app/TextToCanvas';
 
 type Data = {
@@ -12,13 +13,21 @@ export default async function handler(
 ) {
   if (req.method !== 'POST') return res.status(500);
 
-  const maxNameLength = 20;
+  const MAX_TEXT_LENGTH = 20;
+  const POSITION_X = 0;
+  const POSITION_Y = 80;
+  const FONT_SIZE = 76;
+  const FILL = 'red';
+  const STORKE = 'red';
+
   const { text }: { text: string } = req.body;
   const regex = /[&'`"<>]/g;
   if (regex.test(text)) return res.status(500);
-  if (!(text.length <= maxNameLength)) return res.status(500);
+  if (!(text.length <= MAX_TEXT_LENGTH)) return res.status(500);
 
   const font = await opentype.load('fonts/NotoSansJP-Regular.otf');
-  const path = font.getPath(text, 0, 75, 72);
+  const path = font.getPath(text, POSITION_X, POSITION_Y, FONT_SIZE);
+  path.fill = FILL;
+  path.stroke = STORKE;
   res.status(200).json({ path });
 }
