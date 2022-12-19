859f75110a8cb7f0f662d3d79285104b7f0b5ed2
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-19 17:18:14 +0900

    移動ロジックを分離

--- src/app/Canvas.tsx
+++ src/app/Canvas.tsx
@@ -1,33 +1,14 @@
 'use client';
-import { useEffect, FC, useRef, memo, useState } from 'react';
+import { FC, memo, useEffect } from 'react';
 import { pathDraw } from '../commons/pathDraw';
+import { useCanvas } from '../hooks/useCanvas';
 import { TPath } from './TextToCanvas';
 
 export const Canvas: FC<{
   textPath: TPath | null;
   isLoading: boolean;
 }> = memo(({ textPath, isLoading }) => {
-  const canvas = useRef<HTMLCanvasElement | null>(null);
-  const canvasCtx = useRef<CanvasRenderingContext2D | null>(null);
-  
-  const positionX = useRef(0);
-  const positionY = useRef(0);
-  const initialX = useRef(0);
-  const initialY = useRef(0);
-  const [offsetX, setOffsetX] = useState(0);
-  const [offsetY, setOffsetY] = useState(0);
-
-  useEffect(() => {
-    if (canvas.current === null) return;
-    canvas.current.width = 500;
-    canvas.current.height = 300;
-    canvasCtx.current = canvas.current.getContext('2d');
-    return () => {
-      canvas.current?.removeEventListener('mousemove', handleMove);
-      canvas.current?.removeEventListener('mouseout', handleOut);
-      canvas.current?.removeEventListener('mouseup', handleUp);
-    };
-  }, []);
+  const { canvas, canvasCtx, offsetX, offsetY } = useCanvas(textPath);
 
   useEffect(() => {
     if (canvas.current === null) return;
@@ -37,54 +18,18 @@ export const Canvas: FC<{
     canvasCtx.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
     pathDraw({ ctx: canvasCtx.current, path: textPath, offsetX, offsetY });
   }, [textPath, offsetX, offsetY]);
-
-  function handleDown(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
-    initialX.current = event.screenX;
-    initialY.current = event.screenY;
-
-    canvas.current?.addEventListener('mousemove', handleMove);
-    canvas.current?.addEventListener('mouseup', handleUp);
-    canvas.current?.addEventListener('mouseout', handleOut);
-  }
-
-  function handleMove(event: MouseEvent) {
-    const isMovableX = !event.shiftKey || event.altKey;
-    const isMovableY = !event.shiftKey || !event.altKey;
-
-    if (isMovableX) {
-      setOffsetX(positionX.current + event.screenX - initialX.current);
-      positionX.current += event.screenX - initialX.current;
-      initialX.current = event.screenX;
-    }
-    if (isMovableY) {
-      setOffsetY(positionY.current + event.screenY - initialY.current);
-      positionY.current += event.screenY - initialY.current;
-      initialY.current = event.screenY;
-    }
-  }
-
-  function handleUp(event: MouseEvent) {
-    canvas.current?.removeEventListener('mousemove', handleMove);
-    canvas.current?.removeEventListener('mouseout', handleOut);
-    canvas.current?.removeEventListener('mouseup', handleUp);
-  }
-
-  function handleOut(event: MouseEvent) {
-    canvas.current?.removeEventListener('mousemove', handleMove);
-    canvas.current?.removeEventListener('mouseup', handleUp);
-    canvas.current?.removeEventListener('mouseout', handleOut);
-  }
-
+  
   return (
-    <div id="field">
+    <div>
       {isLoading && <p>通信中...</p>}
       <canvas
-        ref={canvas}
+        id="canvas"
         style={{
           display: isLoading ? 'none' : undefined,
           backgroundColor: '#E6E6E6',
         }}
-        onMouseDown={handleDown}
+        width={500}
+        height={300}
       />
       <p>Shiftキーを押すと上下移動</p>
       <p>Shift + Altで左右移動</p>
---
+++ src/hooks/useCanvas.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
+import { TPath } from '../app/TextToCanvas';
+import { pathDraw } from '../commons/pathDraw';
+
+export const useCanvas = (textPath: TPath | null) => {
+  const canvas = useRef<HTMLCanvasElement | null>(null);
+  const canvasCtx = useRef<CanvasRenderingContext2D | null>(null);
+
+  const positionX = useRef(0);
+  const positionY = useRef(0);
+  const initialX = useRef(0);
+  const initialY = useRef(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
+
+  useEffect(() => {
+    canvas.current = document.getElementById('canvas') as HTMLCanvasElement | null;
+    if (canvas.current === null) return;
+    canvas.current.addEventListener('mousedown', handleDown);
+    canvasCtx.current = canvas.current.getContext('2d');
+    return () => {
+      if (canvas.current === null) return;
+      canvas.current.removeEventListener('mousedown', handleDown);
+      canvas.current.removeEventListener('mousemove', handleMove);
+      canvas.current.removeEventListener('mouseout', handleOut);
+      canvas.current.removeEventListener('mouseup', handleUp);
+    };
+  }, []);
+
+  function handleDown(event: MouseEvent) {
+    initialX.current = event.screenX;
+    initialY.current = event.screenY;
+
+    canvas.current?.addEventListener('mousemove', handleMove);
+    canvas.current?.addEventListener('mouseup', handleUp);
+    canvas.current?.addEventListener('mouseout', handleOut);
+  }
+
+  function handleMove(event: MouseEvent) {
+    const isMovableX = !event.shiftKey || event.altKey;
+    const isMovableY = !event.shiftKey || !event.altKey;
+
+    if (isMovableX) {
+      setOffsetX(positionX.current + event.screenX - initialX.current);
+      positionX.current += event.screenX - initialX.current;
+      initialX.current = event.screenX;
+    }
+    if (isMovableY) {
+      setOffsetY(positionY.current + event.screenY - initialY.current);
+      positionY.current += event.screenY - initialY.current;
+      initialY.current = event.screenY;
+    }
+  }
+
+  function handleUp(event: MouseEvent) {
+    canvas.current?.removeEventListener('mousemove', handleMove);
+    canvas.current?.removeEventListener('mouseout', handleOut);
+    canvas.current?.removeEventListener('mouseup', handleUp);
+  }
+
+  function handleOut(event: MouseEvent) {
+    canvas.current?.removeEventListener('mousemove', handleMove);
+    canvas.current?.removeEventListener('mouseup', handleUp);
+    canvas.current?.removeEventListener('mouseout', handleOut);
+  }
+  return { canvas, canvasCtx, offsetX, offsetY };
+};
