10f377e086066c80c770f914b7374df9e0732e06
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-19 12:47:55 +0900

    不要コメントを削除

--- src/app/Canvas.tsx
+++ src/app/Canvas.tsx
@@ -184,38 +184,3 @@ const pathDraw = ({
     ctx.stroke();
   }
 };
-
-// const pathDraw = ({
-//   ctx,
-//   path,
-// }: {
-//   ctx: CanvasRenderingContext2D;
-//   path: TPath;
-// }) => {
-//   ctx.beginPath();
-//   for (let i = 0; i < path.commands.length; i += 1) {
-//     const cmd = path.commands[i];
-//     if (cmd.type === 'M') {
-//       ctx.moveTo(cmd.x, cmd.y);
-//     } else if (cmd.type === 'L') {
-//       ctx.lineTo(cmd.x, cmd.y);
-//     } else if (cmd.type === 'C') {
-//       ctx.bezierCurveTo(cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
-//     } else if (cmd.type === 'Q') {
-//       ctx.quadraticCurveTo(cmd.x1, cmd.y1, cmd.x, cmd.y);
-//     } else if (cmd.type === 'Z') {
-//       ctx.closePath();
-//     }
-//   }
-
-//   if (path.fill) {
-//     ctx.fillStyle = path.fill;
-//     ctx.fill();
-//   }
-
-//   if (path.stroke) {
-//     ctx.strokeStyle = path.stroke;
-//     ctx.lineWidth = path.strokeWidth;
-//     ctx.stroke();
-//   }
-// };
