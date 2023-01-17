75908bada6b4be997be8dca374ebaf61d967de6b
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-17 09:34:11 +0900

    同名リスナを複数登録できる形に変更

--- src/hooks/useCanvas.tsx
+++ src/hooks/useCanvas.tsx
@@ -17,7 +17,7 @@ type HooksArg = {
 type canvasEventKey = 'pointermove' | 'pointerdown' | 'pointerup' | 'pointerout' | 'pointerover';
 type CanvasEvent = (event: globalThis.PointerEvent) => any;
 type CanvasEvents = {
-  [key in canvasEventKey]?: CanvasEvent;
+  [key in canvasEventKey]?: CanvasEvent[];
 };
 
 type CanvasState = 'searchPath' | 'movePath' | 'dragArea' | 'movePathOut' | 'dragAreaOut';
@@ -69,35 +69,39 @@ export const useCanvas = ({ textPaths, setTextPaths }: HooksArg) => {
 
   const canvasEventList: CanvasEventsList = {
     searchPath: {
-      pointermove: searchPath_Move,
-      pointerdown: searchPath_Down,
+      pointermove: [searchPath_Move],
+      pointerdown: [searchPath_Down],
     },
     movePath: {
-      pointermove: movePath_Move,
-      pointerup: movePath_Up,
-      pointerout: movePath_Out,
+      pointermove: [movePath_Move],
+      pointerup: [movePath_Up],
+      pointerout: [movePath_Out],
     },
     dragArea: {
-      pointermove: dragArea_Move,
-      pointerup: dragArea_Up,
-      pointerout: dragArea_Out,
+      pointermove: [dragArea_Move],
+      pointerup: [dragArea_Up],
+      pointerout: [dragArea_Out],
     },
-    movePathOut: { pointerover: mouvePathOver_Over },
-    dragAreaOut: { pointerover: dragAreaOut_Over },
+    movePathOut: { pointerover: [mouvePathOver_Over] },
+    dragAreaOut: { pointerover: [dragAreaOut_Over] },
   };
 
   const [canvasState, setCanvasState] = useState<CanvasState>('searchPath');
 
   useEffect(() => {
-    const addEvents = Object.entries(canvasEventList[canvasState]) as [canvasEventKey, CanvasEvent][];
-    addEvents.forEach(([key, value]) => {
-      canvas.current?.addEventListener(key, value, false);
+    const addEvents = Object.entries(canvasEventList[canvasState]) as [canvasEventKey, CanvasEvent[]][];
+    addEvents.forEach(([key, values]) => {
+      values.forEach((value) => {
+        canvas.current?.addEventListener(key, value);
+      });
     });
 
     return () => {
-      const removeEvents = Object.entries(canvasEventList[canvasState]) as [canvasEventKey, CanvasEvent][];
-      removeEvents.forEach(([key, value]) => {
-        canvas.current?.removeEventListener(key, value, false);
+      const removeEvents = Object.entries(canvasEventList[canvasState]) as [canvasEventKey, CanvasEvent[]][];
+      removeEvents.forEach(([key, values]) => {
+        values.forEach((value) => {
+          canvas.current?.removeEventListener(key, value);
+        });
       });
     };
   }, [textPaths, canvasState]);
