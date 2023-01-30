3c3fbce5d9ab0717fc124176ce6c305fa181c63c
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-30 10:21:37 +0900

    TextToCanvasを廃止してpageへ移植

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Suspense } from 'react';
-import { TextToCanvas } from './TextToCanvas';
+'use client';
+
+import { useTextToCanvas } from '../hooks/useTextToCanvas';
+import { Canvas } from './Canvas';
+import { TextInput } from './TextInput';
 
 export default function Page() {
+  const { inputText, isLoading, textPaths, changeInput, addText2Path, setTextPaths } = useTextToCanvas();
+
   return (
-    <div style={{ padding: '16px', height: '95%' }} id="page">
-      <TextToCanvas />
+    <div id="page" style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+      <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} />
+      <Canvas textPaths={textPaths} setTextPaths={setTextPaths} />
     </div>
   );
 }
