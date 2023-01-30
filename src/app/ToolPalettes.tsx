fbcc6b4fd8f35ca9d5038e3c60c7119bff6e93b4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-30 12:55:53 +0900

    ツールパレットの土台を作成

---
+++ src/app/ToolPalettes.tsx
@@ -0,0 +1,14 @@
+import styles from './toolpalettes.module.css';
+
+export const ToolPalettes = () => {
+  return (
+    <div className={styles.toolpalettes}>
+      <div className={styles.tool}>
+        <div>↖︎</div>
+      </div>
+      <div className={styles.tool}>
+        <div>T</div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
+import { useState } from 'react';
 import { useTextToCanvas } from '../hooks/useTextToCanvas';
 import { Canvas } from './Canvas';
-import { TextInput } from './TextInput';
+import { ToolPalettes } from './ToolPalettes';
 
 export default function Page() {
   const { inputText, isLoading, textPaths, changeInput, addText2Path, setTextPaths } = useTextToCanvas();
 
   return (
-    <div id="page" style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-      <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} />
+    <div id="page" style={{ display: 'flex', flexDirection: 'column' }}>
+      {/* <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} /> */}
+      <ToolPalettes />
       <Canvas textPaths={textPaths} setTextPaths={setTextPaths} />
     </div>
   );
