e02f9d154d0d6d8b609104fdfae81f4fc3e3d030
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-30 10:05:18 +0900

    suspenseを廃止

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { TextToCanvas } from './TextToCanvas';
 export default function Page() {
   return (
     <div style={{ padding: '16px', height: '95%' }} id="page">
-      <Suspense fallback={<p>システムローディング中</p>}>
-        <TextToCanvas />
-      </Suspense>
+      <TextToCanvas />
     </div>
   );
 }
