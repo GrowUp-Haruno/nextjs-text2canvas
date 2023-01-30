0b2704b1b75e1ce53bc2c25c2e65bf96868ff3c4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-30 10:08:16 +0900

    操作説明を一時退避

--- src/app/TextToCanvas.tsx
+++ src/app/TextToCanvas.tsx
@@ -3,7 +3,6 @@ import { memo } from 'react';
 import { Canvas } from './Canvas';
 import { useTextToCanvas } from '../hooks/useTextToCanvas';
 import { TextInput } from './TextInput';
-import { Explain } from './Explain';
 
 export const TextToCanvas = memo(() => {
   const { inputText, isLoading, textPaths, changeInput, addText2Path, setTextPaths } = useTextToCanvas();
@@ -12,7 +11,6 @@ export const TextToCanvas = memo(() => {
     <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
       <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} />
       <Canvas textPaths={textPaths} setTextPaths={setTextPaths} />
-      <Explain />
     </div>
   );
 });
