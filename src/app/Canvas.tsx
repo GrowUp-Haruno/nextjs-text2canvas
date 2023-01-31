64dbc89e41ebddfcd8c92287a2fffe189791cea5
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-01-31 09:30:41 +0900

    コンポーネント配置を見直し

--- src/app/Canvas.tsx
+++ src/app/Canvas.tsx
@@ -1,32 +1,17 @@
 'use client';
 import { FC, memo } from 'react';
-import { useCanvas } from '../hooks/useCanvas';
-import { useTextToCanvas } from '../hooks/useTextToCanvas';
-import { TextPath } from '../types/TextPath';
-import { TextInput } from './TextInput';
-import { ToolPalettes } from './ToolPalettes';
 
 export const Canvas: FC = memo(() => {
-  const { inputText, isLoading, textPaths, changeInput, addText2Path, setTextPaths } = useTextToCanvas();
-  useCanvas({
-    textPaths,
-    setTextPaths,
-  });
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} />
-      {/* <ToolPalettes /> */}
-      <div id="canvasWrap" style={{ width: '100%', height: '100%', backgroundColor: '#E6E6E6'}}>
-        <canvas
-          id="canvas"
-          style={{
-            backgroundColor: '#E6E6E6',
-          }}
-          width={1000}
-          height={400}
-        />
-      </div>
+    <div id="canvasWrap" style={{ width: '100%', height: '100%', backgroundColor: '#E6E6E6' }}>
+      <canvas
+        id="canvas"
+        style={{
+          backgroundColor: '#E6E6E6',
+        }}
+        width={10}
+        height={10}
+      />
     </div>
   );
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+'use client';
+import { useCanvas } from '../hooks/useCanvas';
+import { useTextToCanvas } from '../hooks/useTextToCanvas';
 import { Canvas } from './Canvas';
+import { TextInput } from './TextInput';
+import { ToolPalettes } from './ToolPalettes';
 
 export default function Page() {
-  return <Canvas />;
+  // const { inputText, isLoading, textPaths, changeInput, addText2Path, setTextPaths } = useTextToCanvas();
+  // useCanvas({
+  //   textPaths,
+  //   setTextPaths,
+  // });
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100vw', height: '100vh' }}>
+      <ToolPalettes />
+      {/* <TextInput inputText={inputText} isLoading={isLoading} changeInput={changeInput} addText2Path={addText2Path} /> */}
+      <Canvas />
+    </div>
+  );
 }
